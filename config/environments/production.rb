488203139f6f0c82cb68016b485246468175f783
nanako kan <n.k.0213.0529@gmail.com>
2020-11-09 07:25:23 +0900

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'sheltered-harbor-42574.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'desolate-scrubland-76949.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
