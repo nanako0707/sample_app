4aa2d8f0ad59d2b7ae18d17ef17bae98ca2297f4
nanako kan <n.k.0213.0529@gmail.com>
2020-11-08 17:08:03 +0900

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://damp-sands-43250.herokuapp.com'
+  host = 'https://quiet-lowlands-65825.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
